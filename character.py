2b2bbc8fd87b0cdc257fd742ac9beaac7e13ede7
Umer-1-14-debug <m.umer0000013@gmail.com>
2025-10-15 17:31:57 +0500

    Add files via upload

---
+++ character.py
@@ -0,0 +1,5 @@
+string=input("Enter any word ")
+character=input("Enter any word ")
+for x in string:
+    if x==character:
+        break
---
+++ pass.py
@@ -0,0 +1,14 @@
+for x in range(10):
+    if x%20:
+        print("Completed")
+    elif x%10:
+        pass
+    elif x%15:
+        print("Condition completed")
+    elif x%5:
+        print("Condition completed")
+    elif x%3:
+        print("Condition completed")
+    else:
+        print("Condition not completed")
+    
