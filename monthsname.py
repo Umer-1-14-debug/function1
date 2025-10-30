5f890693cdf12ba7ae1824d41b4cd0d4d50dbd53
Umer-1-14-debug <m.umer0000013@gmail.com>
2025-10-30 15:50:44 +0500

    Add files via upload

---
+++ monthsname.py
@@ -0,0 +1,20 @@
+def months():
+    choice=input("Do you want to see the names of months in a year")
+    if choice=="Yes":
+        print("January")
+        print("Febuary")
+        print("March")
+        print("April")
+        print("May")
+        print("June")
+        print("July")
+        print("Agust")
+        print("September")
+        print("Octuber")
+        print("November")
+        print("December")
+    elif choice=="No":
+        print("Ok")
+    else:
+        print("Try again")
+months()
